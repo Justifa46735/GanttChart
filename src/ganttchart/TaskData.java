1aee581e36bbb715ad19a6801c8730f4225fec7b
lenovo <lenovo@x230t>
2014-11-21 13:32:11 +0100

    TaskData Klasse: zwei Konstruktoren zur Übergabe des start_dates und der dauer eingefügt.

--- src/ganttchart/TaskData.java
+++ src/ganttchart/TaskData.java
@@ -33,6 +33,20 @@ public class TaskData
         this.startDate = stringToDate(startDate);
         this.endDate = stringToDate(endDate);
     }
+    
+    public TaskData(String bezeichnung, Date startDate, int dauer)
+    {
+        this.bezeichnung = bezeichnung;
+        this.startDate = startDate;
+        this.endDate = new Date(this.startDate.getTime() + dauer);
+    }
+    
+    public TaskData(String bezeichnung, String startDate, int dauer) throws ParseException
+    {
+        this.bezeichnung = bezeichnung;
+        this.startDate = stringToDate(startDate);
+        this.endDate = new Date(this.startDate.getTime() + dauer);
+    }
 
     private Date stringToDate(String date) throws ParseException
     {
